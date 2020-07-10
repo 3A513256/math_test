f057109827c192ced96f2e27ac59a0fc104d5535
3A513256 <alone916916@gmail.com>
2020-07-11 01:02:33 +0800

    V5.0

--- app/src/main/java/com/Beta/math_practice/Qusetion.java
+++ app/src/main/java/com/Beta/math_practice/Qusetion.java
@@ -38,35 +38,26 @@ public class Qusetion {
         return question;
     }
 
-    ;
 
     public String getChoice1(int a) {
         String choice = mChoices[a][0];
         return choice;
     }
 
-    ;
-
     public String getChoice2(int a) {
         String choice = mChoices[a][1];
         return choice;
     }
-
-    ;
 
     public String getChoice3(int a) {
         String choice = mChoices[a][2];
         return choice;
     }
 
-    ;
-
     public String getChoice4(int a) {
         String choice = mChoices[a][3];
         return choice;
     }
-
-    ;
 
     public String getCorrectAnswer(int a) {
         String answer = mCorrectAnswers[a];
